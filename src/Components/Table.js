d80dc5bae3af98a2cbd07f1291c008bc14dfc8aa
junaidirfan1 <junaidirfan1998@icloud.com>
2023-10-30 18:14:21 +0500

    2nd Commit

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -8,7 +8,7 @@ function Table() {
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
               <th scope="col" className="px-3 py-3">
-                 Account ID
+                 Account ID Please
                 </th>
                 <th scope="col" className="px-1 ">
                  Date
